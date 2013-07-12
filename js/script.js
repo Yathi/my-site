39a232d7232dd3c6388ee6c5e717d33a6b15428a
Yathi <bm.thunderemperor@gmail.com>
2013-07-12 19:40:09 +0530

    Updated the js and added effects

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-$document.ready(function() {
-}
+$(document).ready(function() {
+    $('article:first-child').addClass('visible').fadeIn(1000);
+
+    $('#android-rooted-list a').click(function() {
+        $('article.visible').removeClass('visible').removeClass('gayab').fadeOut(500);
+        $('li.active').removeClass('active');
+        $('#android-rooted').addClass('visible').fadeIn(1000);
+        $('#android-rooted-list').addClass('active');
+        
+        
+    });
+    $('#no-task-killer-list a').click(function() {
+        $('article.visible').removeClass('visible').removeClass('gayab').fadeOut(500);
+        $('li.active').removeClass('active');
+        $('#no-task-killer').removeClass('gayab').addClass('visible').fadeIn(1000);
+        $('#no-task-killer-list').addClass('active');
+    });
+});
