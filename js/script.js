1511a2d55a60b66a61e10093ca4bd19d9514995f
Yathi <bm.thunderemperor@gmail.com>
2013-07-15 10:00:05 +0530

    Added the social buttons from add this site

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('article:first-child').addClass('visible').fadeIn(1000);
 
     $('#android-rooted-list a').click(function() {
-        $('article.visible').removeClass('visible').removeClass('gayab').fadeOut(500);
+        $('article.visible').removeClass('visible').removeClass('gayab').hide();
         $('li.active').removeClass('active');
         $('#android-rooted').addClass('visible').fadeIn(1000);
         $('#android-rooted-list').addClass('active');
@@ -10,7 +10,7 @@ $(document).ready(function() {
         
     });
     $('#no-task-killer-list a').click(function() {
-        $('article.visible').removeClass('visible').removeClass('gayab').fadeOut(500);
+        $('article.visible').removeClass('visible').removeClass('gayab').hide();
         $('li.active').removeClass('active');
         $('#no-task-killer').removeClass('gayab').addClass('visible').fadeIn(1000);
         $('#no-task-killer-list').addClass('active');
